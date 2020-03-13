ceed2f8fad5beb606c11e6b93c18a1e0f8fb57d4
hector lopez <hectorvmlopez@gmail.com>
2020-03-13 23:29:05 +0000

    fix for 0 and adding b case

--- format.c
+++ format.c
@@ -101,7 +101,8 @@ int print_to_binary(char *buffer, va_list list, int index_buffer)
 	int i;
 	char *p;
 
-	p =  convert(va_arg(list, int), 2);
+
+	p =  convert(va_arg(list, unsigned int), 2);
 	for (i = 0; p[i] != '\0'; ++i, index_buffer++)
 		buffer[index_buffer]  = p[i];
 
