56182a6563a40d3de01141964dc7e3d269bfa8dd
hector lopez <hectorvmlopez@gmail.com>
2020-03-14 14:51:20 +0000

    hector

--- printf_lite.c
+++ printf_lite.c
@@ -12,7 +12,7 @@ int _printf(const char *format, ...)
 	int (*get)(char *, va_list, int);
 	int i = 0, j = 0, j_temp = 0, j_temp_2 = 0, ocurrencias = 0, largo_format;
 
-	va_start(arg, format), buffer = create_buffer(1024);
+	va_start(arg, format), buffer = create_buffer(7000);
 	if (format == NULL || buffer == NULL)
 		return (-1);
 	largo_format = _strlen(format);
@@ -20,22 +20,11 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] != '%')
 		{
-			if (j == 1024)
-			{	write(1, buffer,1023);
-				j = 0, j_temp_2 = 0;
-				buffer[j] = format[i];
-			}
-			else
-			{
-				buffer[j] = format[i];
-			}
+			buffer[j] = format[i];
 		}
 		else
-		{
-			if (j == 1024)
-			{	write(1, buffer, (i - ocurrencias) + (j_temp_2));
-				j = 0, j_temp_2 = 0, ocurrencias = 0, buffer[j] = '\0'; }
-			i++, get = get_format(format[i]);
+		{	i++;
+			get = get_format(format[i]);
 			if (get == NULL)
 			{
 				if ((i == 1 && format[i] == 0))
@@ -46,15 +35,16 @@ int _printf(const char *format, ...)
 					free(buffer), va_end(arg);
 					return (-1);
 				}
-				else
-				{	buffer[j] = format[i - 1], j++, buffer[j] = format[i]; } }
+				else if (format[largo_format] == 10)
+				{	buffer[j] = format[i - 1], j++, buffer[j] = format[i];
+				}
+			}
 			if (get != NULL)
 			{	j_temp  += get(buffer, arg, j), j += j_temp, ocurrencias++;
-				j_temp_2 += j_temp, j_temp = 0; } }
-		i++, j++; }
-	write(1, buffer, (i - ocurrencias) + (j_temp_2)), va_end(arg), free(buffer);
-	return ((i - ocurrencias) + j_temp_2);
-}
-/*	printf(" I %d \n",i) printf(" J %d \n",j)*/
-/*	printf("Ocurrencia %d\n",ocurrencias) printf("Jtemp_2 %d\n",j_temp_2);*/
+				j_temp_2 += j_temp, j_temp = 0;
+			} } i++, j++; }
+	write(1, buffer, (i - ocurrencias) + (j_temp_2));
+	va_end(arg);
+	free(buffer);
+	return ((i - ocurrencias) + j_temp_2); }
 
--- test/test_char_strings.c
+++ test/test_char_strings.c
@@ -10,8 +10,8 @@ int main(void)
 	char *str = NULL;
 	int l2,pepe;
 	l2 = 0;
-
-
+	_printf("Her mom had warned her. She had been warned time and again, but she had refused to believe her. She had done everything right and she knew she would be rewarded for doing so with the promotion. So when the promotion was given to her main rival, it not only stung, it threw her belief system into disarray. It was her first big lesson in life, but not the last.Her mom had warned her. She had been warned time and again, but she had refused to believe her. She had done everything right and she knew she would be rewarded for doing so with the promotion. So when the promotion was given to her main rival, it not only stung, it threw her belief system into disarray. It was her first big lesson in life, but not the last.Her mom had warned her. She had been warned time and again, but she had refused to believe her. She had done everything right and she knew she would be rewarded for doing so with the promotion. So when the promotion was given to her main rival, it not only stung, it threw her belief system into disarray. It was her first big lesson in life, but not the last.Her mom had warned her.it not only stung, it threw her belief system into disarray. It was her first big lesson in life, bl");
+	putchar('\n');
 
 
 
