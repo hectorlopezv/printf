7933703a338ac65918dd2c726908f8a7ce46500f
hector lopez <hectorvmlopez@gmail.com>
2020-03-14 11:27:30 +0000

    fix for 0

--- printf_lite.c
+++ printf_lite.c
@@ -10,12 +10,13 @@ int _printf(const char *format, ...)
 {	va_list arg;
 	char *buffer;
 	int (*get)(char *, va_list, int);
-	int i = 0, j = 0, j_temp = 0, j_temp_2 = 0, ocurrencias = 0;
+	int i = 0, j = 0, j_temp = 0, j_temp_2 = 0, ocurrencias = 0, largo_format;
 
 	va_start(arg, format);
 	buffer = create_buffer(7000);
 	if (format == NULL || buffer == NULL)
 		return (-1);
+	largo_format = _strlen(format);
 	while (format[i] && format)
 	{
 		if (format[i] != '%')
@@ -25,7 +26,13 @@ int _printf(const char *format, ...)
 			if (get == NULL)
 			{
 				if ((i == 1 && format[i] == 0))
-				{	free(buffer), va_end(arg);
+				{
+					free(buffer), va_end(arg);
+					return (-1);
+				}
+				else if (format[largo_format] != 10)
+				{	write(1, buffer, (i - ocurrencias) + (j_temp_2));
+					free(buffer), va_end(arg);
 					return (-1);
 				}
 				else
@@ -38,9 +45,6 @@ int _printf(const char *format, ...)
 	write(1, buffer, (i - ocurrencias) + (j_temp_2)), va_end(arg), free(buffer);
 	return ((i - ocurrencias) + j_temp_2);
 }
-
-/*	printf(" I %d \n",i);*/
-/*	printf(" J %d \n",j);*/
-/*	printf("Ocurrencia %d\n",ocurrencias);*/
-/*	printf("Jtemp_2 %d\n",j_temp_2);*/
+/*	printf(" I %d \n",i) printf(" J %d \n",j)*/
+/*	printf("Ocurrencia %d\n",ocurrencias) printf("Jtemp_2 %d\n",j_temp_2);*/
 
--- test/test_char_strings.c
+++ test/test_char_strings.c
@@ -8,10 +8,85 @@ int main(void)
 	int alen, elen,pelen;
 	char c = 'z';
 	char *str = NULL;
-	int l2;
+	int l2,pepe;
 	l2 = 0;
 
-	l2 = _printf("22%22\n");
+
+
+
+
+
+
+	printf("Let's print a simple sentence.\n");
+	_printf("Let's print a simple sentence.\n");
+
+	printf("%c", 'S');
+	putchar('\n');
+	_printf("%c", 'S');
+	putchar('\n');
+	printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+
+
+	printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+
+	printf("%s", "This sentence is retrieved from va_args!\n");
+	_printf("%s", "This sentence is retrieved from va_args!\n");
+
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+
+	printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+
+	printf("%%");
+	putchar('\n');
+	_printf("%%");
+	putchar('\n');
+
+	printf("Should print a single percent sign: %%\n");
+
+	_printf("Should print a single percent sign: %%\n");
+
+	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+
+	printf("css%ccs%scscscs", 'T', "Test");
+	putchar('\n');
+	_printf("css%ccs%scscscs", 'T', "Test");
+	putchar('\n');
+
+	pepe =_printf(NULL);
+	printf("%d\n",pepe);
+
+	printf("%c", '\0');
+	putchar('\n');
+
+	_printf("%c", '\0');
+	putchar('\n');
+
+	printf("%");
+	putchar('\n');
+
+	_printf("%");
+	putchar('\n');
+
+	printf("%!\n");
+	_printf("%!\n");
+
+	printf("%K\n");
+
+	_printf("%K\n");
+
+
+
+	putchar('\n');
+
+	l2 = _printf("22%22");
 	printf("%d\n",l2);
 
 
@@ -28,9 +103,7 @@ int main(void)
 	printf("Expected: %s\n",(char *)0);
 	_printf("Actual: %s\n",(char *)0);
 
-	alen = printf(NULL);
 	elen = _printf(NULL);
-	printf("tamano null %d\n",alen);
 	printf("tamano null %d\n",elen);
 
 	printf("Expected: %\n");
