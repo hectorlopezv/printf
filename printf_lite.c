5843e0fbb912e14ab6b606bb7ab03fa25287c1b2
Kev0417 <1681@holbertonschool.com>
2020-03-15 21:52:44 -0500

    Edit printf_lite.c

--- printf_lite.c
+++ printf_lite.c
@@ -8,6 +8,34 @@
 
 int _printf(const char *format, ...)
 {
+	va_list arg;
+	char *buffer;
+	int i = 0;
+	void (*get)(char *, va_list);
+
+	va_start(arg, format);
+	buffer = malloc(1024);
+	if (buffer == NULL)
+		return (NULL);
+
+	while (format[i])
+	{
+		if (format[i] != %)
+		{
+			buffer[i] = format[i];
+		}
+		else
+		{
+			i++;
+			get = get_format(format[i]);
+			get(buffer, arg);
+			free(buffer);
+			buffer = malloc(1024);
+		}
+		i++;
+
+	}
+	va_end(arg);
 
 }
 
