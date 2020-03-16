755c5b44e5662f48632428456c23121238303ce1
Kev0417 <1681@holbertonschool.com>
2020-03-15 20:38:28 -0500

    Edit holberton.h file

--- holberton.h
+++ holberton.h
@@ -1,24 +1,14 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
-#include <stdlib.
+#include <stdlib.h>
 #include <stdarg.h>
 #include <stdio.h>
 
 	struct formats[] = {
-		{'d', print_d},
-		{'i', print_d},
-		{'c', print_c},
-		{'s', print_c},
-		{'%', print_percent},
-		{'b', print_b},
-		{'r', print_r},
-		{'R', print_rot13},
-		{'u', print_u},
-		{'o', print_o},
-		{'x', print_x},
-		{'X', print_X},
-		{'S', print_S},
-		{'p', print_p},
+		{'d', print_int},
+		{'i', print_int},
+		{'c', print_char},
+		{'s', print_str},
 		{'\0', NULL}
 	};
 typedef struct formats format;
