f95987a0610198dda13155a9008b8a1af3b59113
Kev0417 <1681@holbertonschool.com>
2020-03-16 00:17:40 -0500

    Add _strlen.c file

---
+++ _strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen - return the length of the string
+ * @s: char to check
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+
+	return (a);
+}
