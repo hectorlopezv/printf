a6c40bcd4a78eba271cc38c75264f0dc73f14ac1
Kev0417 <1681@holbertonschool.com>
2020-03-15 21:22:02 -0500

    Edit format.c - add prototypes and commets

---
+++ format.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+void print_int(char *buffer, va_list list)
+{
+}
+
+/**
+ *
+ *
+ */
+void print_str(char *buffer, va_list list)
+{
+}
+
+/**
+ *
+ *
+ */
+void print_char(char *buffer, va_list list)
+{
+}
