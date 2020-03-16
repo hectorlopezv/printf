c1be37ac4f3c4b5cc599d7695d410e08588da78a
Kev0417 <1681@holbertonschool.com>
2020-03-16 12:24:10 -0500

    Edit holberton.h file - betty style

--- holberton.h
+++ holberton.h
@@ -4,18 +4,23 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * struct formats - Struct formats
+ *
+ * @c: the format
+ * @f: The function associated
+ */
 	struct formats
 	{
 		char c;
-		int (*f)(char *, va_list,int a);
-
+		int (*f)(char *, va_list, int a);
 	};
 
 int  print_int(char *buffer, va_list list, int a);
-int print_str(char *buffer, va_list list,int a);
-int print_char(char *buffer, va_list list,int a);
-int print_percentage_literal(char *buffer, va_list list,int a);
-int (*get_format(char format))(char *, va_list,int a);
+int print_str(char *buffer, va_list list, int a);
+int print_char(char *buffer, va_list list, int a);
+int print_percentage_literal(char *buffer, va_list list, int a);
+int (*get_format(char format))(char *, va_list, int a);
 int _printf(const char *format, ...);
 char *convert(long int num, int base);
 
