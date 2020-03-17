506678130a79aca2ef74070ced258547f3c65e70
hector vinicio lopez <46845790+hectorlopezv@users.noreply.github.com>
2020-03-17 01:22:35 -0500

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -28,6 +28,8 @@ char *create_buffer(int size);
 int print_to_binary(char *buffer, va_list list, int a);
 int print_pointer(char *buffer, va_list list, int a);
 int print_rot13(char *buffer, va_list list, int index_buffer);
+int _strlen(char *s);
+int rev_string(char *str);
 
 #endif
 
