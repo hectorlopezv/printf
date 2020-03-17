24a27d07559609e65952bec9055d64ca4e36be65
hector vinicio lopez <46845790+hectorlopezv@users.noreply.github.com>
2020-03-17 01:18:36 -0500

    Update conversion_2.c

--- conversion_2.c
+++ conversion_2.c
@@ -67,3 +67,53 @@ int print_rot13(char *buffer, va_list list, int index_buffer)
 
 	return (i - 1);
 }
+
+/**
+ * rev_string - check the code for Holberton School students.
+ *@str: string character
+ * Return: Always 0.
+ */
+
+void rev_string(char *str)
+{
+	int b, c;
+	int length = _strlen(str);
+
+	char temp;
+
+	b = 0;
+	c = length;
+
+	/*swappp*/
+	while (c >= b)
+	{
+		temp = str[b];
+		str[b] = str[c];
+		str[c] = temp;
+		c--;
+		b++;
+	}
+}
+
+/**
+ * _strlen - check the code for Holberton School students.
+ *@s: string variable
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+	int counter = 0;
+
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		counter++;
+		i++;
+	}
+	counter--;
+	return (counter);
+}
